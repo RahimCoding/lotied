eed8223c14094463a335800a1682ac8ba48d2aee
Rahim Jamal <rahim_92@live.ca>
2022-10-19 16:24:10 -0400

    without

---
+++ without.js
@@ -0,0 +1,66 @@
+function eqArrays(value1, value2) {
+  for (let i = 0; i < value1.length; i++) {
+    if (value1[i] !== value2[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+function assertArraysEqual(array1, array2) {
+  let value1 = array1
+  let value2 = array2
+  eqArrays(value1, value2)
+  if (eqArrays(value1, value2) === true) {
+    console.log("ITS TRUE")
+  } else {
+    console.log("ITS FALSE")
+  }
+}
+
+// function without(originalArray, itemsToRemoveFromArray) {
+//   let newArray = []
+//   let toAdd = false
+//   for (i = 0; i < originalArray.length; i++) {
+//     for (j = 0; j < itemsToRemoveFromArray.length; j++) {
+//       if (itemsToRemoveFromArray !== originalArray) {
+//         toAdd = true
+//       } else {
+//         toAdd = false
+//       }
+//     }
+//     console.log('too add:', originalArray[i])
+//     console.log("will we add it:", toAdd)
+//     if (toAdd) {
+//       newArray.push(originalArray[i])
+//     }
+//   }
+//   console.log(newArray)
+//   return newArray
+// }
+
+function without(originalArray, itemsToRemoveFromArray) {
+  let newArray = []
+  for(i = 0; i < originalArray.length; i++){
+    if(!itemsToRemoveFromArray.includes(originalArray[i])){
+      newArray.push(originalArray[i])
+      
+    }
+  } console.log(newArray)
+  }
+
+
+
+
+
+
+
+
+
+
+
+  const words = ["hello", "world", "lighthouse"];
+  without(words, ["lighthouse"]); // no need to capture return value for this test case
+  // Make sure the original array was not altered by the without function
+  assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
