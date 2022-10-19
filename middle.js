6bac31b41b95e761092b21b035a7e9e0b8044d1d
Rahim Jamal <rahim_92@live.ca>
2022-10-19 18:06:43 -0400

    final edits

--- middle.js
+++ middle.js
@@ -1,8 +1,31 @@
+function eqArrays(value1, value2) {
+  if (value1.length === value2.length) {
+    for (let i = 0; i < value1.length; i++) {
+      if (value1[i] !== value2[i]) {
+        return false
+      }
+    }
+    return true
+  }
+  return false
+}
+
+function assertArraysEqual(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log("ITS TRUE")
+  } else {
+    console.log("ITS FALSE")
+  }
+}
+
+
+
+
 const middle = function(array){
 let result = []
-if(array.length < 1) {
+if(array.length < 3) {
   return []
-}
+} 
 if(array.length % 2 === 1){
 let index = (array.length - 1)/2
 result.push(array[index])
@@ -12,7 +35,15 @@ result.push(array[index])
   result.push(array[index1])
   result.push(array[index2])
 }
+console.log(result)
 return result
 }
 
-console.log(middle([1,2,3,4,5,6]))
+middle([1]) // => []
+middle([1, 2]) // => []
+
+middle([1, 2, 3]) // => [2]
+middle([1, 2, 3, 4, 5]) // => [3]
+
+middle([1, 2, 3, 4]) // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
