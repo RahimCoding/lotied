ebbcc51fd2f5f889dc409d0ec3274e1a89ece49c
Rahim Jamal <rahim_92@live.ca>
2022-10-19 23:48:10 -0400

    findkeybyvalue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${[actual]} === ${[expected]}`);
+  }
+};
+
+const findKeyByValue = function(object, value){
+  //creates array
+  for (let key of Object.keys(object)){
+    //checks the object if its same as value
+    if(object[key] === value){
+      // returns value of key
+      return key;
+    }
+  }
+  //else return undefined
+  return undefined;
+
+}
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
