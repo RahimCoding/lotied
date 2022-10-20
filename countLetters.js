9b7290541aef35ee669870193a5daa3844b09d1a
Rahim Jamal <rahim_92@live.ca>
2022-10-19 20:21:17 -0400

    countletters.js

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${[actual]} === ${[expected]}`);
+  }
+};
+
+const countLetters = function(allItems){
+  let results = {}
+  for (let item of allItems) {
+    // console.log(item);
+    if (item !== " "){
+      if(results[item]){
+        results[item] += 1
+      } else {
+        results[item] = 1
+      }
+    }
+  }
+  return results
+
+}
+// console.log(countLetters('LHL'))
+// countLetters('LHL')
+
+console.log(countLetters("lighthouse in the house"))
