a4a13211604104f11497c50b2eefa0a6c16f5ecc
Rahim Jamal <rahim_92@live.ca>
2022-10-19 21:28:44 -0400

    final edits

--- letterPositions.js
+++ letterPositions.js
@@ -40,21 +40,5 @@ function assertArraysEqual(array1, array2) {
 
 
 
-// assertArraysEqual(letterPositions("hello").e, [1]);
-
-const mockReturn = {
-  l: [0],
-  i: [1, 11],
-  g: [2],
-  h: [3, 5, 15, 18],
-  t: [4, 14],
-  o: [6, 19],
-  u: [7, 20],
-  s: [8, 21],
-  e: [9, 16, 22],
-  n: [12]
-}
-
-// const mockFunction = letterPositions('hello')
 
 assertArraysEqual(letterPositions("hello").e, [1]);
